0386f3f8058d50d89849a34ba9599ee9a1af2e84
fanay <1909509136@qq.com>
2026-06-22 10:19:39 +0800

    '月度账单-切换弹框功能'

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,7 +1,14 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 const Month = () => {
+  const [dateVisible, setDateVisible] = useState(false)
+  const onConfirm = (date) => {
+    console.log(date)
+    setDateVisible(false)
+  }
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -10,11 +17,11 @@ const Month = () => {
       <div className="content">
         <div className="header">
           {/* 时间切换区域 */}
-          <div className="date">
+          <div className="date" onClick={() => setDateVisible(true)}>
             <span className="text">
               2023 | 3月账单
             </span>
-            <span className='arrow expand'></span>
+            <span className={classNames('arrow', dateVisible && 'expand')}></span>
           </div>
           {/* 统计区域 */}
           <div className='twoLineOverview'>
@@ -36,7 +43,10 @@ const Month = () => {
             className="kaDate"
             title="记账日期"
             precision="month"
-            visible={false}
+            visible={dateVisible}
+            onCancel={() => setDateVisible(false)}
+            onConfirm={onConfirm}
+            onClose={() => setDateVisible(false)}
             max={new Date()}
           />
         </div>
